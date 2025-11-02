df3355f6585e1192133f02d59655d777e65e270d
maheshsaini1997 <maheshcsis97@gmail.com>
2025-11-02 15:23:31 +0530

    Bucker Creation

--- Dev/main.tf
+++ Dev/main.tf
@@ -10,8 +10,3 @@ resource "aws_s3_bucket" "my_bucket" {
         Environment = "Dev"
     }
 }
-
-resource "aws_s3_bucket_acl" "bucket_acl" {
-    bucket = aws_s3_bucket.my_bucket.id
-    acl    = "private"
-}
