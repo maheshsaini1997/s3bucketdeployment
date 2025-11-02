5473227bbeeb58d881b371e22342ed0aa348d26b
maheshsaini1997 <maheshcsis97@gmail.com>
2025-11-02 15:20:59 +0530

    Bucker Creation

--- Dev/main.tf
+++ Dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-    bucket = "practice-bucket-1"
+    bucket = "maheshsaini1997-practice-bucket-1"
 
     tags = {
         Name        = "My bucket"
