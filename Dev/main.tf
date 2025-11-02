2ed6f1cd1c2f7a52b0251aa366dff6835a6ff30d
maheshsaini1997 <maheshcsis97@gmail.com>
2025-11-02 15:16:35 +0530

    Initial Push

--- Dev/main.tf
+++ Dev/main.tf
@@ -4,10 +4,14 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
     bucket = "PracticeBucket1"
-    acl    = "private"
 
     tags = {
         Name        = "My bucket"
         Environment = "Dev"
     }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+    bucket = aws_s3_bucket.my_bucket.id
+    acl    = "private"
 }
